d99436555cc42d6afd821d278bf42c78b6271b6f
Jennifer Amanze <jennfranc18@gmail.com>
2026-01-18 22:49:38 +0100

    Update select.jsx

--- src/components/select.jsx
+++ src/components/select.jsx
@@ -2,7 +2,7 @@ export default function SelectField(props) {
   const { placeholder, id, options, value, onSelect } = props
 
   return (<section role="dropdown">
-    <select defaultValue={value} onChange={(e) => onSelect(e.target.value)} list={id} id={id}>
+    <select value={value} onChange={(e) => onSelect(e.target.value)} list={id} id={id}>
       <option value=""> {placeholder}</option>
       {
         options.map((val) => <option key={val} 
